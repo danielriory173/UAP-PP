84177e0ad8b40523aacaa77f9e2be3d0e177439b
danielriory173 <danielriory173@gmail.com>
2025-12-01 17:43:20 +0700

    Initialize projectAkhir.cpp with basic includes
    Mulainnya project kita

---
+++ projectAkhir.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdlib.h>
+#include <windows.h>
+#include <ncurses/ncurses.h>
+#include <unistd.h>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    
+}
