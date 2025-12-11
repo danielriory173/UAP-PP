fdfddf6a21e34a205171156793e3a7126da7c31a
alaskavvv <zbcs9z8z98@privaterelay.appleid.com>
2025-12-11 21:03:44 +0700

    Define game constants and structures
    Added constants and structures for game configuration.

--- projectAkhir.cpp
+++ projectAkhir.cpp
@@ -5,7 +5,17 @@
 #include <unistd.h>
 #include <fstream>
 
-using namespace std;
+#define LEBAR 30
+#define TINGGI 20
+#define UKURAN_ULAR_AWAL3
+#define UKURAN_ULAR_MAKS100
+#define SKOR_BOOSTER 50
+
+struck titik{
+int x,y;
+};
+
+enum JenisMakanan{BIASA,BOOSTER};
 
 int main(){
     
