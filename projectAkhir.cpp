a794a9f13ef2b579872112c69d2ad4211d7afd66
danielriory173 <danielriory173@gmail.com>
2025-12-14 20:04:55 +0700

    semoga UAS digampangkan

--- projectAkhir.cpp
+++ projectAkhir.cpp
@@ -60,9 +60,13 @@ void buatMakanan(){
                 
 
 
-
 }
-
+if (rand() % 5 == 0) {  
+        jenisMakanan = BOOSTER;  
+    } else {  
+        jenisMakanan = BIASA;  
+    }  
+}  
 
 void gambarPapan() {  
     clear();  
@@ -89,3 +93,88 @@ void gambarPapan() {
     mvprintw(1, LEBAR + 2, "Skor: %d", skor);  
     refresh();  
 }  
+
+void gerakUlar() {  
+    Titik kepalaBaru = ular[0];  
+    kepalaBaru.x += arahX;  
+    kepalaBaru.y += arahY;  
+
+    if (kepalaBaru.x == 0 || kepalaBaru.x == LEBAR - 1 || kepalaBaru.y == 0 || kepalaBaru.y == TINGGI - 1) {  
+        permainanSelesai = true;  
+        return;  
+    }  
+    for (int i = 0; i < panjangUlar; ++i) {  
+        if (ular[i].x == kepalaBaru.x && ular[i].y == kepalaBaru.y) {  
+            permainanSelesai = true;  
+            return;  
+        }  
+    }  
+
+    for (int i = panjangUlar - 1; i > 0; --i) {  
+        ular[i] = ular[i - 1];  
+    }  
+
+    ular[0] = kepalaBaru;  
+
+    if (kepalaBaru.x == makanan.x && kepalaBaru.y == makanan.y) {  
+        if (jenisMakanan == BIASA) {  
+            skor += 10;  
+        } else {  
+            skor += SKOR_BOOSTER;  
+        }  
+
+        if (panjangUlar < UKURAN_ULAR_MAKS) {  
+            panjangUlar++;  
+        }  
+        buatMakanan();  
+    }  
+}  
+
+void prosesInput() {  
+    timeout(100);  
+    int masukan = getch();  
+    switch (masukan) {  
+    case KEY_UP:  
+        if (arahY == 0) {  
+            arahX = 0;  
+            arahY = -1;  
+        }  
+        break;  
+    case KEY_DOWN:  
+        if (arahY == 0) {  
+            arahX = 0;  
+            arahY = 1;  
+        }  
+        break;  
+    case KEY_LEFT:  
+        if (arahX == 0) {  
+            arahX = -1;  
+            arahY = 0;  
+        }  
+        break;  
+    case KEY_RIGHT:  
+        if (arahX == 0) {  
+            arahX = 1;  
+            arahY = 0;  
+        }  
+        break;  
+    case 'q':  
+        permainanSelesai = true;  
+        break;  
+    }  
+}  
+
+bool cekPermainanSelesai() {  
+    return permainanSelesai;  
+}  
+
+void jalankan() {  
+    while (!permainanSelesai) {  
+        prosesInput();  
+        gerakUlar();  
+        gambarPapan();  
+    }  
+}
+
+};
+
