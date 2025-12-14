c7f098193384aea430d3ea33f11aadb72d3a1eb1
alaskavvv <zbcs9z8z98@privaterelay.appleid.com>
2025-12-14 09:55:16 +0700

    Update projectAkhir.cpp

--- projectAkhir.cpp
+++ projectAkhir.cpp
@@ -7,8 +7,8 @@
 
 #define LEBAR 30
 #define TINGGI 20
-#define UKURAN_ULAR_AWAL3
-#define UKURAN_ULAR_MAKS100
+#define UKURAN_ULAR_AWAL 3
+#define UKURAN_ULAR_MAKS 100
 #define SKOR_BOOSTER 50
 
 struck titik{
@@ -16,6 +16,27 @@ int x,y;
 };
 
 enum JenisMakanan{BIASA,BOOSTER};
+
+class PermainanUlar{
+private:
+int skor;
+Titik ular [UKURAN_ULAR_MAKS];
+
+int panjangUlar;
+Titik makanan;
+JenisMakanan jenisMakanan;
+int arah X, int arah Y;
+bool permainanSelesai;
+std::string namaPemain;
+
+public:
+PermainanUlar(const std::string&nama) {
+skor = 0;
+arah X = 1;
+arah Y = 0;
+permainanSlesai = false;
+panjangUlar = UKURAN_ULAR_AWAL;
+namaPemain = nama;
 
 int main(){
     
